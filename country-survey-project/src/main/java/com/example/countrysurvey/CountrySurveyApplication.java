6fbcd8b037d4c8318929fdd602606fa7cf334238
Victoria Rose <rad.vicisrad@gmail.com>
2024-03-08 19:35:01 -0500

    IntelliJ project created

---
+++ country-survey-project/src/main/java/com/example/countrysurvey/CountrySurveyApplication.java
@@ -0,0 +1,13 @@
+package com.example.countrysurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrySurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountrySurveyApplication.class, args);
+	}
+
+}
---
+++ country-survey-project/src/test/java/com/example/countrysurvey/CountrySurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countrysurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrySurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
