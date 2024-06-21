5bbbb7530897bb7ee121b325a4194265f6e308d2
Victoria Rose <rad.vicisrad@gmail.com>
2024-06-20 21:30:42 -0400

    added two more queries

---
+++ server/database/Queries/Group_Stats/all_participants_from_highest_count_to_lowest.sql
@@ -0,0 +1,6 @@
+SELECT r.participant_id, CONCAT (p.first_name,' ',p.last_name) AS participant_name, COUNT (*) AS countries_remembered
+FROM results r 
+JOIN participant p
+ON r.participant_id = p.participant_id
+GROUP BY r.participant_id, participant_name
+ORDER BY countries_remembered DESC;
--- server/database/Queries/Individual_Stats/countries_i_remembered_most_to_least_common.sql
+++ server/database/Queries/Individual_Stats/countries_i_remembered_most_to_least_common.sql
@@ -1,10 +1,7 @@
-SELECT country_name
-FROM country c JOIN results r
-ON c.country_id = r.country_id
-WHERE country_name IN 
-	(SELECT c.country_name, COUNT(*) AS number_of_guesses
-	FROM results r
-	INNER JOIN country c
-	ON r.country_id = c.country_id
-	GROUP BY c.country_name
-	ORDER BY number_of_guesses DESC)
+SELECT c.country_name, COUNT(*) AS number_of_guesses
+FROM results r
+INNER JOIN country c
+ON r.country_id = c.country_id
+WHERE r.country_id IN (SELECT country_id FROM results WHERE participant_id = 1)
+GROUP BY c.country_name
+ORDER BY number_of_guesses DESC;
